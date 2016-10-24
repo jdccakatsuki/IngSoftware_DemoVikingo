8a832c7c7fbb73c87ed50d56258013e12d2fd9ce
Daniel Cano <josecastilloc93@gmail.com>
2016-10-24 17:52:42 -0500

    Initial Commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnManager : MonoBehaviour {
+
+    //Singleton
+
+    private static SpawnManager instance;
+    public static SpawnManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    //Class
+
+    void SpawnObject(GameObject target)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VikingHealth.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VikingHealth : MonoBehaviour {
+
+    [SerializeField]
+    private float health;
+    [SerializeField]
+    private float customDamage;
+    private float healthTemp;
+
+    [Header("Referencias")]
+
+    [SerializeField]
+    private Text gameOverText;
+    private SkinnedMeshRenderer[] objectRenderer = new SkinnedMeshRenderer[3];
+
+    void Start()
+    {
+        objectRenderer = GetComponentsInChildren<SkinnedMeshRenderer>();
+        healthTemp = health;
+    }
+
+    void Update()
+    {
+        Debug.Log(health);
+
+        if (health <= 0)
+        {
+            gameOverText.text = "GAME OVER";
+            Disapear();
+        }
+
+        //TEST
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            MakeDamage(customDamage);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Restart();
+        }
+    }
+
+    private void MakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    private void Disapear()
+    {
+        for(int i = 0; i < objectRenderer.Length; i++)
+        {
+            objectRenderer[i].enabled = false;
+        }
+    }
+
+    private void Restart()
+    {
+        for (int i = 0; i < objectRenderer.Length; i++)
+        {
+            objectRenderer[i].enabled = true;
+        }
+
+        health = healthTemp;
+        gameOverText.text = "";
+    }
+}
